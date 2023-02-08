f8363ff156ccfefc66168a976c54d1157a39d524
soheeKang <thgml352@gmail.com>
2023-02-08 22:59:49 +0900

    Initial commit

---
+++ src/main/java/com/daangn/secondhanddealings/SecondHandDealingsApplication.java
@@ -0,0 +1,13 @@
+package com.daangn.secondhanddealings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondHandDealingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondHandDealingsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/daangn/secondhanddealings/SecondHandDealingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daangn.secondhanddealings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondHandDealingsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
